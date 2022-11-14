ce941b84b90c5a93905fb80c07fd91c2542f031e
willdebras <wbonnell123@gmail.com>
2022-11-13 21:19:13 -0600

    larger sandbox

--- src/Sandbox.js
+++ src/Sandbox.js
@@ -4,47 +4,53 @@ import * as THREE from 'three'
 THREE.ColorManagement.legacyMode = false
 
 const boxGeometry = new THREE.BoxGeometry(1, 1, 1)
-const floor1Material = new THREE.MeshStandardMaterial({ color: '#fff', metalness: 0, roughness: 0 })
+const floor1Material = new THREE.MeshStandardMaterial({ color: 'lightgreen', metalness: 0, roughness: 0 })
 const wallMaterial = new THREE.MeshStandardMaterial({ color: '#887777', metalness: 0, roughness: 0 })
 
 export default function Sandbox()
 {
     return <group>
         <RigidBody type="fixed" restitution={ 0.2 } friction={ 0 }>
-            <mesh geometry={ boxGeometry } position={ [ 0, -0.1, 0 ] } scale={ [ 50, 0.2, 50 ] } material={ floor1Material } receiveShadow />
+            <mesh 
+                geometry={ boxGeometry } 
+                position={ [ 0, -0.1, 0 ] } 
+                scale={ [ 100, 0.2, 100 ] } 
+                material={ floor1Material } 
+                receiveShadow 
+            />
         </RigidBody>
         <RigidBody type="fixed" restitution={ 0.2 } friction={ 0 }>
             <mesh 
                 geometry={ boxGeometry } 
-                position={ [ 0, 1.5, -25 ] } 
+                position={ [ 0, 1.5, -50 ] } 
                 material={ wallMaterial }
-                scale={ [ 50, 3, 1 ] }
+                scale={ [ 100, 3, 1 ] }
                 castShadow
             />
             <mesh 
                 geometry={ boxGeometry } 
-                position={ [ 0, 1.5, 25 ] } 
+                position={ [ 0, 1.5, 50 ] } 
                 material={ wallMaterial }
-                scale={ [ 50, 3, 1 ] }
+                scale={ [ 100, 3, 1 ] }
                 castShadow
             />
             <mesh 
                 geometry={ boxGeometry } 
-                position={ [ -25, 1.5, 0 ] } 
+                position={ [ -50, 1.5, 0 ] } 
                 material={ wallMaterial }
-                scale={ [ 1, 3, 50 ] }
+                scale={ [ 1, 3, 100 ] }
                 castShadow
             />
             <mesh 
                 geometry={ boxGeometry } 
-                position={ [ 25, 1.5, 0 ] } 
+                position={ [ 50, 1.5, 0 ] } 
                 material={ wallMaterial }
-                scale={ [ 1, 3, 50 ] }
+                scale={ [ 1, 3, 100 ] }
                 castShadow
             />
         </RigidBody>
         <CuboidCollider 
-            args={ [ 50, 0.2, 50 ] }
+            args={ [ 100, 0.2, 100 ] }
             position={ [ 0, -0.1, 0 ] }
             restitution={ 0.2 }
             friction={ 1 }
